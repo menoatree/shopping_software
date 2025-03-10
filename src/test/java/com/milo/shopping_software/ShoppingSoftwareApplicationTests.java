b399d370d5933131930ebdb5dc2fe9bbcf467139
Milo <milo@localhost.de>
2025-03-10 17:35:35 +0100

    working app with connected db

---
+++ src/test/java/com/milo/shopping_software/ShoppingSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.shopping_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
